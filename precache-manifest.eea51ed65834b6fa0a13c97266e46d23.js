9e98832f8cad762b7ae19a6e63a9fd7197857835
Vishal Sharma <vishalagnihotri345@gmail.com>
2019-03-19 14:22:01 +0530

    Add files via upload

---
+++ precache-manifest.eea51ed65834b6fa0a13c97266e46d23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "79fb50735d01c97ec73d",
+    "url": "/portfolio/static/css/main.d927c6cf.chunk.css"
+  },
+  {
+    "revision": "79fb50735d01c97ec73d",
+    "url": "/portfolio/static/js/main.b3870553.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/portfolio/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6f3edbc7e1b286e2e99bb333e91668dc",
+    "url": "/portfolio/index.html"
+  }
+];
